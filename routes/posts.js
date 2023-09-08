4b8c452a4d5157b0f1d8e8e13e4c73c970537470
Naufal Rizq Ramadhan <91309270+naufalrizq@users.noreply.github.com>
2023-09-08 16:54:16 +0700

    feat(input data): create FE & BE

--- routes/posts.js
+++ routes/posts.js
@@ -24,4 +24,75 @@ router.get('/', function (req, res, next) {
     });
 });
 
+/**
+ * CREATE POST
+ */
+router.get('/create', function (req, res, next) {
+    res.render('posts/create', {
+        title: '',
+        content: ''
+    })
+})
+
+/**
+ * STORE POST
+ */
+router.post('/store', function (req, res, next) {
+
+    let title = req.body.title;
+    let content = req.body.content;
+    let errors = false;
+
+    if (title.length === 0) {
+        errors = true;
+
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Title");
+        // render to add.ejs with flash message
+        res.render('posts/create', {
+            title: title,
+            content: content
+        })
+    }
+
+    if (content.length === 0) {
+        errors = true;
+
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Konten");
+        // render to add.ejs with flash message
+        res.render('posts/create', {
+            title: title,
+            content: content
+        })
+    }
+
+    // if no error
+    if (!errors) {
+
+        let formData = {
+            title: title,
+            content: content
+        }
+
+        // insert query
+        connection.query('INSERT INTO posts SET ?', formData, function (err, result) {
+            //if(err) throw err
+            if (err) {
+                req.flash('error', err)
+
+                // render to add.ejs
+                res.render('posts/create', {
+                    title: formData.title,
+                    content: formData.content
+                })
+            } else {
+                req.flash('success', 'Data Berhasil Disimpan!');
+                res.redirect('/posts');
+            }
+        })
+    }
+
+})
+
 module.exports = router;

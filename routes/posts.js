92849473c445fc1d45694bae9281160540d0b1ea
Naufal Rizq Ramadhan <91309270+naufalrizq@users.noreply.github.com>
2023-09-08 16:50:52 +0700

    add(dbConnection,Post Route,Posts View): initial commit

---
+++ routes/posts.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+//import database
+var connection = require('../library/database');
+
+/**
+ * INDEX POSTS
+ */
+router.get('/', function (req, res, next) {
+    //query
+    connection.query('SELECT * FROM posts ORDER BY id desc', function (err, rows) {
+        if (err) {
+            req.flash('error', err);
+            res.render('posts', {
+                data: ''
+            });
+        } else {
+            //render ke view posts index
+            res.render('posts/index', {
+                data: rows // <-- data posts
+            });
+        }
+    });
+});
+
+module.exports = router;

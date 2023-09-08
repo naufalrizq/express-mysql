89f9d4ecfc7bb1a95a4be61b3b6399de6ef19bab
Naufal Rizq Ramadhan <91309270+naufalrizq@users.noreply.github.com>
2023-09-08 17:04:24 +0700

    merge develop to main (#1)
    * add(dbConnection,Post Route,Posts View): initial commit
    
    * feat(input data): create FE & BE
    
    * feat(update posts):initial commit
    
    * feat(delete post):initial commit
    
    * refacto(whole project): cleaning code
    
    * add(publicjs): initial commit

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//library
+var flash = require('express-flash');
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts'); // <-- route posts
 
 var app = express();
 
@@ -19,16 +24,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  cookie: {
+    maxAge: 60000
+  },
+  store: new session.MemoryStore,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'secret'
+}))
+
+app.use(flash())
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter); // use route posts di Express
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ routes/posts.js
@@ -0,0 +1,213 @@
+var express = require('express');
+var router = express.Router();
+
+//import database
+var connection = require('../library/database');
+
+/**
+ * INDEX POSTS
+ */
+router.get('/', function (req, res, next) {
+    //query
+    connection.query('SELECT * FROM posts ORDER BY id desc', function (err, rows) {
+        if (err) {
+            req.flash('error', err);
+            res.render('posts', {
+                data: ''
+            });
+        } else {
+            //render ke view posts index
+            res.render('posts/index', {
+                data: rows // <-- data posts
+            });
+        }
+    });
+});
+
+/**
+ * CREATE POST
+ */
+router.get('/create', function (req, res, next) {
+    res.render('posts/create', {
+        title: '',
+        content: ''
+    })
+})
+
+/**
+ * STORE POST
+ */
+router.post('/store', function (req, res, next) {
+
+
+    let title = req.body.title;
+    let content = req.body.content;
+    let errors = false;
+
+    if (title.length === 0) {
+        errors = true;
+
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Title");
+        // render to add.ejs with flash message
+        res.render('posts/create', {
+            title: title,
+            content: content
+        })
+    }
+
+    if (content.length === 0) {
+        errors = true;
+
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Konten");
+        // render to add.ejs with flash message
+        res.render('posts/create', {
+            title: title,
+            content: content
+        })
+    }
+
+    // if no error
+    if (!errors) {
+
+        let formData = {
+            title: title,
+            content: content
+        }
+
+        // insert query
+        connection.query('INSERT INTO posts SET ?', formData, function (err, result) {
+            //if(err) throw err
+            if (err) {
+                req.flash('error', err)
+
+                // render to add.ejs
+                res.render('posts/create', {
+                    title: formData.title,
+                    content: formData.content
+                })
+            } else {
+                req.flash('success', 'Data Berhasil Disimpan!');
+                res.redirect('/posts');
+            }
+        })
+    }
+
+})
+
+/**
+ * EDIT POST
+ */
+router.get('/edit/(:id)', function (req, res, next) {
+
+    let id = req.params.id;
+
+    connection.query('SELECT * FROM posts WHERE id = ' + id, function (err, rows, fields) {
+        if (err) throw err
+
+        // if user not found
+        if (rows.length <= 0) {
+            req.flash('error', 'Data Post Dengan ID ' + id + " Tidak Ditemukan")
+            res.redirect('/posts')
+        }
+        // if book found
+        else {
+            // render to edit.ejs
+            res.render('posts/edit', {
+                id: rows[0].id,
+                title: rows[0].title,
+                content: rows[0].content
+            })
+        }
+    })
+})
+
+/**
+ * UPDATE POST
+ */
+router.post('/update/:id', function (req, res, next) {
+
+    let id = req.params.id;
+    let title = req.body.title;
+    let content = req.body.content;
+    let errors = false;
+
+    if (title.length === 0) {
+        errors = true;
+
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Title");
+        // render to edit.ejs with flash message
+        res.render('posts/edit', {
+            id: req.params.id,
+            title: title,
+            content: content
+        })
+    }
+
+    if (content.length === 0) {
+        errors = true;
+
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Konten");
+        // render to edit.ejs with flash message
+        res.render('posts/edit', {
+            id: req.params.id,
+            title: title,
+            content: content
+        })
+    }
+
+    // if no error
+    if (!errors) {
+
+        let formData = {
+            title: title,
+            content: content
+        }
+
+        // update query
+        connection.query('UPDATE posts SET ? WHERE id = ' + id, formData, function (err, result) {
+            //if(err) throw err
+            if (err) {
+                // set flash message
+                req.flash('error', err)
+                // render to edit.ejs
+                res.render('posts/edit', {
+                    id: req.params.id,
+                    name: formData.name,
+                    author: formData.author
+                })
+            } else {
+                req.flash('success', 'Data Berhasil Diupdate!');
+                res.redirect('/posts');
+            }
+        })
+    }
+})
+
+/**
+ * DELETE POST
+ */
+router.get('/delete/(:id)', function (req, res, next) {
+
+    let id = req.params.id;
+
+    connection.query('DELETE FROM posts WHERE id = ' + id, function (err, result) {
+        //if(err) throw err
+        if (err) {
+            // set flash message
+            req.flash('error', err)
+            // redirect to posts page
+            res.redirect('/posts')
+        } else {
+            // set flash message
+            req.flash('success', 'Data Berhasil Dihapus!')
+            // redirect to posts page
+            res.redirect('/posts')
+        }
+    })
+})
+
+module.exports = router;
